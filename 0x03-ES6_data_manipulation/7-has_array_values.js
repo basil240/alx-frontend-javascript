85ad6268be0b37810560c91292077c0c3274fe96
Ekpei <iekpei@yahoo.com>
2023-10-02 16:53:40 +0100

    More set data structure

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,22 @@
+import hasValuesFromArray from "./7-has_array_values.js";
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1]));
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [10]));
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1, 10]));
+
+<hi>More set data structure</hi>
+
+<body>
+
+    <script>
+        let x = ["val-1", "val-2", "val-3", "val-4", "val-5"]
+        //console.log(x);
+        process_array(x);
+
+        function process_array(params){
+
+            for(i-0; i< URLSearchParams, lenght; i++){}
+            console.log(params[i] + "-processing");
+        }
+
+    </script>
+</body>
