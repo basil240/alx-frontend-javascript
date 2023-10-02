daeee6e37080d7c35911454eb4f077388c5263ce
Ekpei <iekpei@yahoo.com>
2023-10-02 16:32:02 +0100

    Set data structure

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,18 @@
+console.log(setFromArray([12, 32, 15, 78, 98, 15]));
+import setFromArray from "./6-set.js";
+<body>
+    int add(int b[], int len)
+    {
+        int sum = 0, i;
+        for(i=0; i<len; i++)
+            sum += b[i]
+        return sum;
+    }
+
+    int main() {
+        int a[] = {12, 32, 15, 78, 98, 15}
+        int len = sizesof(a)/sizeof(a[0]);
+        printf("%d", add(a, len));
+        return 0;
+    }
+</body>
