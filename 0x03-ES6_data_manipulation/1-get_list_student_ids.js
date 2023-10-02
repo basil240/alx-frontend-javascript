5cb3083ad7b5ba0bd4c9e8b5fa5c791a59e356ff
Ekpei <iekpei@yahoo.com>
2023-10-02 14:28:00 +0100

    my java map

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,15 @@
+console.log(getListStudent("hello"))
+<h1>More mapping</h1>
+<script>
+    let x = ["vol-1","vol-2","vol-3"]
+    //console.log(x);
+    process_array(x);
+
+    function process_array(params){
+        console.log(params);
+        
+        for(i-0; i < params, lenght; i++){
+            console.log(params[i] + "-processing");
+        }
+    }
+</script>
