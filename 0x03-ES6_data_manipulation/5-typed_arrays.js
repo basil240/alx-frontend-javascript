ea1aa07e7676630bf34286e466c47f379324dbd4
Ekpei <iekpei@yahoo.com>
2023-10-02 16:17:58 +0100

    Typed Arrays

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,13 @@
+import createInt8TypedArray from "./5-typed_arrays.js";
+console.log(createInt8TypedArray(86, 172, 430));
+<body>
+    let values = [86,172,430];
+
+    values.push(5);
+
+    console.log(values[2]);
+    
+
+
+
+</body>
