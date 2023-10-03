c4559573e2a3d97960e2ad46a5cff1cc025298d0
Ekpei <iekpei@yahoo.com>
2023-10-03 13:47:22 +0100

    Map data structure

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+console.log(groceriesList());
+def groceriesList():
+grocery_items = {
+    "Guillaume": 1,
+    "James": 2,
+    "Serena" : 5,
+}
+return grocery_items
+grocery_list =grocerieslist()
+print('guillaume': 1, 'james': 2, 'Serena': 5,)
+
